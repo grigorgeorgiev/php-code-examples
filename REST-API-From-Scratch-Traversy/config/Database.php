cf1439dd67491ee95d5aa9a025d3289d493788e9
Grigor Georgiev <contact@grigorgeorgiev.com>
2022-06-23 10:22:06 +0300

    Update Database.php

--- REST-API-From-Scratch-Traversy/config/Database.php
+++ REST-API-From-Scratch-Traversy/config/Database.php
@@ -13,12 +13,14 @@
      $this->conn = null;
       
      try {
-      $this->conn = new PDO('mysql:host=' . $this->host . ';dbname= ' . $this->db_name,
-                            $this->username, $this->password);
+       $this->conn = new PDO('mysql:host=' . $this->host . ';dbname= ' . $this->db_name,
+       $this->username, $this->password);
        $this->conn->setAttribute(PDO:ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     } catch(PDOException $e_ {
-      echo 'Connection Error: ' . $e->getMessage();
+     } catch(PDOException $e) {
+       echo 'Connection Error: ' . $e->getMessage();
      }
+      
+      return $this->conn;
     }
  
   
